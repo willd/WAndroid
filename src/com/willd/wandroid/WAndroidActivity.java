64551502d218fade23cad54285275f8e9a418747
Wilhelm Westermark <willd@kth.se>
2012-04-16 16:04:23 +0200

    Fixed past service

--- src/com/willd/wandroid/WAndroidActivity.java
+++ src/com/willd/wandroid/WAndroidActivity.java
@@ -47,7 +47,7 @@ public class WAndroidActivity extends Activity {
         CharSequence contentTitle = "My notification";
         CharSequence contentText = "Hello World!";
         Intent notificationintent = new Intent(this, WAndroidNotification.class);
-        
+        final Intent serviceintent = new Intent(this, WAndroidService.class);
         PendingIntent contentIntent = PendingIntent.getActivity(context, 0, notificationintent, PendingIntent.FLAG_CANCEL_CURRENT);
         
 
@@ -72,14 +72,7 @@ public class WAndroidActivity extends Activity {
         });
         start.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v ) {
-            	tv.setText(R.string.start_text);
-            	if(start.getText().equals(getText(R.string.start_text)))
-            		start.setText(R.string.stop_text);
-            	else
-            		start.setText(R.string.start_text);
-            	
-
-            	setContentView(R.layout.sec);
+            	startService(serviceintent);
             
             }
         });
